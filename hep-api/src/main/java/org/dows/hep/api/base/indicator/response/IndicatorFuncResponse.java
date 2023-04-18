d3b622db8c2b3ec2d008af08c6a56292237724a8
lait <lait.zhang@gmail.com>
2023-04-18 10:54:15 +0800

    fix

---
+++ hep-api/src/main/java/org/dows/hep/api/base/indicator/response/IndicatorFuncResponse.java
@@ -0,0 +1,17 @@
+package org.dows.hep.api.base.indicator.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+* @description 
+*
+* @author 
+* @date 
+*/
+@Data
+@NoArgsConstructor
+@Schema(name = "IndicatorFunc 对象", title = "指标功能列表")
+public class IndicatorFuncResponse{
+}
