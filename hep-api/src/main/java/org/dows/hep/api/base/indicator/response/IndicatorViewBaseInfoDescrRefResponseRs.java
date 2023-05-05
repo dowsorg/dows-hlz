d05daba0a40fa2efca70b218b46f8f087a545d2c
runsix <runsix1994@gmail.com>
2023-05-05 17:42:32 +0800

    20230505 finish

---
+++ hep-api/src/main/java/org/dows/hep/api/base/indicator/response/IndicatorViewBaseInfoDescrRefResponseRs.java
@@ -0,0 +1,39 @@
+package org.dows.hep.api.base.indicator.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @author runsix
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class IndicatorViewBaseInfoDescrRefResponseRs implements Serializable {
+  @JsonFormat(shape = JsonFormat.Shape.STRING)
+  @Schema(title = "主键")
+  private Long id;
+
+  @Schema(title = "分布式ID")
+  private String indicatorViewBaseInfoDescRefId;
+
+  @Schema(title = "应用ID")
+  private String appId;
+
+  @Schema(title = "指标描述表ID")
+  private String indicatorViewBaseInfoDescId;
+
+  @Schema(title = "指标")
+  private IndicatorInstanceResponseRs indicatorInstanceResponseRs;
+
+  @Schema(title = "展示顺序")
+  private Integer seq;
+
+}
