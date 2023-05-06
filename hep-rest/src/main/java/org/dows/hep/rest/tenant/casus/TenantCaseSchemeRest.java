779756294265ed6ffbc95e718e24edd005759b89
lait <lait.zhang@gmail.com>
2023-05-06 13:28:00 +0800

    fix

--- hep-rest/src/main/java/org/dows/hep/rest/tenant/casus/TenantCaseSchemeRest.java
+++ hep-rest/src/main/java/org/dows/hep/rest/tenant/casus/TenantCaseSchemeRest.java
@@ -4,11 +4,13 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.dows.hep.api.tenant.casus.request.CaseSchemePageRequest;
 import org.dows.hep.api.tenant.casus.request.CaseSchemeRequest;
 import org.dows.hep.api.tenant.casus.response.CaseSchemePageResponse;
 import org.dows.hep.api.tenant.casus.response.CaseSchemeResponse;
 import org.dows.hep.biz.tenant.casus.TenantCaseSchemeBiz;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,6 +22,7 @@ import java.util.List;
 * @author lait.zhang
 * @date 2023年4月17日 下午8:00:11
 */
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @Tag(name = "案例方案设计", description = "案例方案设计")
@@ -90,6 +93,22 @@ public class TenantCaseSchemeRest {
     @GetMapping("v1/tenantCasus/caseScheme/disabledCaseScheme")
     public Boolean disabledCaseScheme(@Validated String caseSchemeId) {
         return tenantCaseSchemeBiz.disabledCaseScheme(caseSchemeId);
+    }
+
+
+
+
+    /**
+     * 禁用案例方案
+     * @param
+     * @return
+     */
+    @Operation(summary = "禁用案例方案")
+    @GetMapping("v1/tenantCasus/caseScheme/page")
+    public Boolean disabledCaseScheme(@Validated PageRequest pageRequest) {
+        log.info("分页请求对象：{}",pageRequest);
+        return Boolean.TRUE;
+        //return tenantCaseSchemeBiz.disabledCaseScheme(caseSchemeId);
     }
 
     /**
