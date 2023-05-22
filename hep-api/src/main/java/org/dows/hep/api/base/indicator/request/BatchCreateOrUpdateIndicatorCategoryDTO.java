2833866cdf8800d4a7212a9bc7cec3a1bb636fcc
runsix <runsix1994@gmail.com>
2023-05-22 17:06:59 +0800

    optimize finish

--- hep-api/src/main/java/org/dows/hep/api/base/indicator/request/BatchCreateOrUpdateIndicatorCategoryDTO.java
+++ hep-api/src/main/java/org/dows/hep/api/base/indicator/request/BatchCreateOrUpdateIndicatorCategoryDTO.java
@@ -1,5 +1,6 @@
 package org.dows.hep.api.base.indicator.request;
 
+import io.swagger.annotations.ApiModelProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,8 +13,10 @@ import lombok.NoArgsConstructor;
 @Schema(name = "BatchCreateOrUpdateIndicatorCategory 对象", title = "批量创建或修改指标类别对象")
 public class BatchCreateOrUpdateIndicatorCategoryDTO {
   @Schema(title = "指标类别分布式id")
+  @ApiModelProperty(value = "本次新增（那就不存在）或修改（存在）的指标类别的id")
   private String indicatorCategoryId;
 
   @Schema(title = "指标类别名称")
+  @ApiModelProperty(required = true, value = "指标类别的名称")
   private String categoryName;
 }
