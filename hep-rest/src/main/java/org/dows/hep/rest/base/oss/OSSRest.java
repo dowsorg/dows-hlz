bbf3f3b5943a9fc0295c92601f77794581700128
jiangxia <15228798301@163.com>
2023-05-26 14:12:25 +0800

    add /v1/file/upload

--- hep-rest/src/main/java/org/dows/hep/rest/base/oss/OSSRest.java
+++ hep-rest/src/main/java/org/dows/hep/rest/base/oss/OSSRest.java
@@ -4,16 +4,13 @@ import cn.hutool.core.util.StrUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.dows.framework.api.Response;
 import org.dows.framework.oss.api.OssInfo;
-import org.dows.framework.oss.local.LocalOssClient;
 import org.dows.hep.biz.base.oss.OSSBiz;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Objects;
@@ -31,8 +28,8 @@ public class OSSRest {
         if (Objects.isNull(file) || file.isEmpty()) {
             throw new IOException("文件不能为空");
         }
-        if (file.getSize() > 209715200L) {
-            throw new IOException("文件超过最大限制2M");
+        if (file.getSize() > 629145600L) {
+            throw new IOException("文件超过最大限制6M");
         }
 
         // 文件名后缀
