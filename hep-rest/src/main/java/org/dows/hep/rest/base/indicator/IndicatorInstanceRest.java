12a4e339d47ef9b1eb4d3572c01a669199bab786
runsix <runsix1994@gmail.com>
2023-06-01 15:04:07 +0800

    optimize finish

--- hep-rest/src/main/java/org/dows/hep/rest/base/indicator/IndicatorInstanceRest.java
+++ hep-rest/src/main/java/org/dows/hep/rest/base/indicator/IndicatorInstanceRest.java
@@ -3,6 +3,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.dows.hep.api.base.indicator.request.CreateOrUpdateIndicatorInstanceRequestRs;
+import org.dows.hep.api.base.indicator.request.UpdateIndicatorInstanceMoveRequestRs;
 import org.dows.hep.api.base.indicator.response.IndicatorInstanceCategoryResponseRs;
 import org.dows.hep.biz.base.indicator.IndicatorInstanceBiz;
 import org.springframework.validation.annotation.Validated;
@@ -45,6 +46,12 @@ public class IndicatorInstanceRest {
     @PutMapping("v1/baseIndicator/indicatorInstance/batchUpdateFood")
     public void batchUpdateFood(@RequestBody List<String> indicatorInstanceIdList) {
         indicatorInstanceBiz.batchUpdateFood(indicatorInstanceIdList);
+    }
+
+    @Operation(summary = "上移下移功能")
+    @PostMapping("v1/baseIndicator/indicatorInstance/move")
+    public void move(@RequestBody UpdateIndicatorInstanceMoveRequestRs updateIndicatorInstanceMoveRequestRs) throws InterruptedException {
+        indicatorInstanceBiz.move(updateIndicatorInstanceMoveRequestRs);
     }
 
     @Operation(summary = "根据appId查询出所有的指标")
