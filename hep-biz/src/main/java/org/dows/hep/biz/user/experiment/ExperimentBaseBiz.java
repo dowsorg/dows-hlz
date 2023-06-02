22e27bd306d43023200f04b77a80166a6398c982
fenghaibin <2283839410@qq.com>
2023-06-02 14:40:56 +0800

    评估问卷报告

---
+++ hep-biz/src/main/java/org/dows/hep/biz/user/experiment/ExperimentBaseBiz.java
@@ -0,0 +1,43 @@
+package org.dows.hep.biz.user.experiment;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+import org.dows.account.util.JwtUtil;
+import org.dows.hep.api.enums.EnumToken;
+import org.dows.hep.entity.ExperimentInstanceEntity;
+import org.dows.hep.service.ExperimentInstanceService;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * @author fhb
+ * @description
+ * @date 2023/5/30 15:50
+ */
+@AllArgsConstructor
+@Service
+public class ExperimentBaseBiz {
+    private final ExperimentInstanceService experimentInstanceService;
+
+    public String getCaseInstanceId(String experimentInstanceId) {
+        return Optional.ofNullable(experimentInstanceService.lambdaQuery()
+                        .eq(ExperimentInstanceEntity::getExperimentInstanceId, experimentInstanceId)
+                        .one())
+                .map(ExperimentInstanceEntity::getCaseInstanceId)
+                .orElse("");
+    }
+
+    public String getAccountId(HttpServletRequest request) {
+        String token = request.getHeader("token");
+        Map<String, Object> map = JwtUtil.parseJWT(token, EnumToken.PROPERTIES_JWT_KEY.getStr());
+        return map.get("accountId").toString();
+    }
+
+    public String getAccountName(HttpServletRequest request) {
+        String token = request.getHeader("token");
+        Map<String, Object> map = JwtUtil.parseJWT(token, EnumToken.PROPERTIES_JWT_KEY.getStr());
+        return map.get("accountName").toString();
+    }
+}
