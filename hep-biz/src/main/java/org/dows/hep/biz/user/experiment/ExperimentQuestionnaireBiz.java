8de3f534f2a8d0fec0f986375958edb78b195ac5
fenghaibin <2283839410@qq.com>
2023-06-05 16:48:25 +0800

    实验知识答题

--- hep-biz/src/main/java/org/dows/hep/biz/user/experiment/ExperimentQuestionnaireBiz.java
+++ hep-biz/src/main/java/org/dows/hep/biz/user/experiment/ExperimentQuestionnaireBiz.java
@@ -2,16 +2,21 @@ package org.dows.hep.biz.user.experiment;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.lang.Assert;
+import cn.hutool.core.util.StrUtil;
 import lombok.AllArgsConstructor;
 import org.dows.framework.api.exceptions.BizException;
 import org.dows.hep.api.base.question.request.QuestionSectionResultRequest;
+import org.dows.hep.api.base.question.response.QuestionSectionResponse;
+import org.dows.hep.api.tenant.casus.CaseESCEnum;
+import org.dows.hep.api.tenant.casus.response.CaseQuestionnaireResponse;
 import org.dows.hep.api.user.experiment.ExperimentESCEnum;
 import org.dows.hep.api.user.experiment.request.ExperimentQuestionnaireRequest;
 import org.dows.hep.api.user.experiment.response.ExperimentQuestionnaireResponse;
+import org.dows.hep.biz.base.question.QuestionSectionBiz;
 import org.dows.hep.biz.base.question.QuestionSectionResultBiz;
-import org.dows.hep.entity.ExperimentParticipatorEntity;
+import org.dows.hep.entity.CaseQuestionnaireEntity;
 import org.dows.hep.entity.ExperimentQuestionnaireEntity;
-import org.dows.hep.service.ExperimentParticipatorService;
+import org.dows.hep.service.CaseQuestionnaireService;
 import org.dows.hep.service.ExperimentQuestionnaireService;
 import org.springframework.stereotype.Service;
 
@@ -26,7 +31,8 @@ import java.util.Optional;
 @Service
 public class ExperimentQuestionnaireBiz {
     private final QuestionSectionResultBiz questionSectionResultBiz;
-    private final ExperimentParticipatorService experimentParticipatorService;
+    private final QuestionSectionBiz questionSectionBiz;
+    private final CaseQuestionnaireService caseQuestionnaireService;
     private final ExperimentQuestionnaireService experimentQuestionnaireService;
 
     /**
@@ -36,25 +42,42 @@ public class ExperimentQuestionnaireBiz {
      * @description
      * @date 2023/6/3 20:53
      */
-    public ExperimentQuestionnaireResponse getQuestionnaire(String experimentInstanceId, String periods, String experimentGroupId, String experimentAccountId) {
+    public ExperimentQuestionnaireResponse getQuestionnaire(String experimentInstanceId, String periods, String experimentOrgId, String experimentGroupId, String experimentAccountId) {
         Assert.notNull(experimentInstanceId, ExperimentESCEnum.PARAMS_NON_NULL.getDescr());
         Assert.notNull(periods, ExperimentESCEnum.PARAMS_NON_NULL.getDescr());
+        Assert.notNull(experimentOrgId, ExperimentESCEnum.PARAMS_NON_NULL.getDescr());
         Assert.notNull(experimentGroupId, ExperimentESCEnum.PARAMS_NON_NULL.getDescr());
         Assert.notNull(experimentAccountId, ExperimentESCEnum.PARAMS_NON_NULL.getDescr());
 
-        // 根据账号获取机构
-        ExperimentParticipatorEntity participatorEntity = experimentParticipatorService.lambdaQuery()
-                .eq(ExperimentParticipatorEntity::getAccountId, experimentAccountId)
-                .eq(ExperimentParticipatorEntity::getExperimentInstanceId, experimentInstanceId)
+        // 根据实验id、期数、小组id， 机构id 获取知识答题
+        ExperimentQuestionnaireEntity entity = experimentQuestionnaireService.lambdaQuery()
+                .eq(ExperimentQuestionnaireEntity::getExperimentInstanceId, experimentAccountId)
+                .eq(ExperimentQuestionnaireEntity::getPeriods, periods)
+                .eq(ExperimentQuestionnaireEntity::getExperimentOrgId, experimentOrgId)
+                .eq(ExperimentQuestionnaireEntity::getExperimentGroupId, experimentAccountId)
                 .oneOpt()
-                .orElse(null);
-        if (BeanUtil.isEmpty(participatorEntity)) {
+                .orElseThrow(() -> new BizException(ExperimentESCEnum.QUESTIONNAIRE_NOT_NULL));
 
+        ExperimentQuestionnaireResponse result = new ExperimentQuestionnaireResponse();
+        result.setExperimentQuestionnaireId(entity.getExperimentQuestionnaireId());
+        String caseQuestionnaireId = entity.getCaseQuestionnaireId();
+        String questionSectionResultId = entity.getQuestionSectionResultId();
+        // 无 Result
+        if (StrUtil.isBlank(questionSectionResultId)) {
+            CaseQuestionnaireResponse questionnaireResponse = getCaseQuestionnaireResponse(caseQuestionnaireId);
+            result.setCaseQuestionnaireResponse(questionnaireResponse);
+            return result;
+        }
+        QuestionSectionResponse questionSectionResult = questionSectionResultBiz.getQuestionSectionResult(questionSectionResultId);
+        if (BeanUtil.isEmpty(questionSectionResult)) {
+            CaseQuestionnaireResponse questionnaireResponse = getCaseQuestionnaireResponse(caseQuestionnaireId);
+            result.setCaseQuestionnaireResponse(questionnaireResponse);
+            return result;
         }
 
-
-        // 根据实验id、期数、小组id， 机构id 获取知识答题
-        ExperimentQuestionnaireResponse result = new ExperimentQuestionnaireResponse();
+        // 有 Result
+        CaseQuestionnaireResponse questionnaireResponse = getCaseQuestionnaireResponse(caseQuestionnaireId, questionSectionResult);
+        result.setCaseQuestionnaireResponse(questionnaireResponse);
         return result;
     }
 
@@ -83,5 +106,38 @@ public class ExperimentQuestionnaireBiz {
                 .eq(ExperimentQuestionnaireEntity::getExperimentQuestionnaireId, experimentSchemeId)
                 .set(ExperimentQuestionnaireEntity::getQuestionSectionResultId, questionSectionResultId)
                 .update();
+    }
+
+    private CaseQuestionnaireResponse getCaseQuestionnaireResponse(String caseQuestionnaireId) {
+        return getCaseQuestionnaireResponse0(caseQuestionnaireId, null);
+    }
+
+    private CaseQuestionnaireResponse getCaseQuestionnaireResponse(String caseQuestionnaireId, QuestionSectionResponse questionSectionResult) {
+        return getCaseQuestionnaireResponse0(caseQuestionnaireId, questionSectionResult);
+    }
+
+    private CaseQuestionnaireResponse getCaseQuestionnaireResponse0(String caseQuestionnaireId, QuestionSectionResponse questionSectionResult) {
+        CaseQuestionnaireEntity entity = getById(caseQuestionnaireId);
+        if (BeanUtil.isEmpty(entity)) {
+            throw new BizException(CaseESCEnum.DATA_NULL);
+        }
+        CaseQuestionnaireResponse result = BeanUtil.copyProperties(entity, CaseQuestionnaireResponse.class);
+
+        // set question-section
+        String questionSectionId = entity.getQuestionSectionId();
+        if (BeanUtil.isEmpty(questionSectionResult)) {
+            QuestionSectionResponse questionSectionResponse = questionSectionBiz.getQuestionSection(questionSectionId);
+            result.setQuestionSectionResponse(questionSectionResponse);
+        } else {
+            result.setQuestionSectionResponse(questionSectionResult);
+        }
+
+        return result;
+    }
+
+    private CaseQuestionnaireEntity getById(String caseQuestionnaireId) {
+        return caseQuestionnaireService.lambdaQuery()
+                .eq(CaseQuestionnaireEntity::getCaseQuestionnaireId, caseQuestionnaireId)
+                .one();
     }
 }
