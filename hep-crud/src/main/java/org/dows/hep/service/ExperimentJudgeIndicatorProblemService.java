e534fb81f07678d4291f8dc95612c3ff216f1c0e
jiangxia <15228798301@163.com>
2023-06-05 13:52:38 +0800

    add ExperimentJudgeIndicatorProblem

---
+++ hep-crud/src/main/java/org/dows/hep/service/ExperimentJudgeIndicatorProblemService.java
@@ -0,0 +1,11 @@
+package org.dows.hep.service;
+
+import org.dows.framework.crud.mybatis.MybatisCrudService;
+import org.dows.hep.entity.ExperimentJudgeIndicatorProblemEntity;
+
+/**
+ * @author jx
+ * @date 2023/6/5 13:49
+ */
+public interface ExperimentJudgeIndicatorProblemService extends MybatisCrudService<ExperimentJudgeIndicatorProblemEntity> {
+}
