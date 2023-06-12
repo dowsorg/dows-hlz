7ca417f658ae2dd6acbc2f48b35369746f6db63d
runsix <runsix1994@gmail.com>
2023-06-12 16:19:59 +0800

    weekend finish

--- hep-biz/src/main/java/org/dows/hep/biz/base/indicator/IndicatorExpressionItemBiz.java
+++ hep-biz/src/main/java/org/dows/hep/biz/base/indicator/IndicatorExpressionItemBiz.java
@@ -1,10 +1,15 @@
 package org.dows.hep.biz.base.indicator;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.dows.hep.api.base.indicator.response.IndicatorExpressionItemResponseRs;
+import org.dows.hep.api.enums.EnumESC;
+import org.dows.hep.api.exception.IndicatorExpressionItemException;
 import org.dows.hep.entity.IndicatorExpressionItemEntity;
+import org.dows.hep.service.IndicatorExpressionItemService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Objects;
 
@@ -15,6 +20,8 @@ import java.util.Objects;
 @RequiredArgsConstructor
 @Slf4j
 public class IndicatorExpressionItemBiz {
+
+  private final IndicatorExpressionItemService indicatorExpressionItemService;
   public static IndicatorExpressionItemResponseRs indicatorExpressionItem2ResponseRs(IndicatorExpressionItemEntity indicatorExpressionItemEntity) {
     if (Objects.isNull(indicatorExpressionItemEntity)) {
       return null;
@@ -37,5 +44,17 @@ public class IndicatorExpressionItemBiz {
         .deleted(indicatorExpressionItemEntity.getDeleted())
         .dt(indicatorExpressionItemEntity.getDt())
         .build();
+  }
+
+  @Transactional(rollbackFor = Exception.class)
+  public void delete(String indicatorExpressionItemId) {
+    boolean isRemove = indicatorExpressionItemService.remove(
+        new LambdaQueryWrapper<IndicatorExpressionItemEntity>()
+            .eq(IndicatorExpressionItemEntity::getIndicatorExpressionItemId, indicatorExpressionItemId)
+    );
+    if (!isRemove) {
+      log.warn("method IndicatorExpressionItemBiz.delete indicatorExpressionItemId:{} is illegal", indicatorExpressionItemId);
+      throw new IndicatorExpressionItemException(EnumESC.VALIDATE_EXCEPTION);
+    }
   }
 }
