2168571a8064ac754eba456da81ae787797feb8d
jiangxia <15228798301@163.com>
2023-06-15 13:59:17 +0800

    add crowsInstance

---
+++ hep-crud/src/main/java/org/dows/hep/service/CrowdsInstanceService.java
@@ -0,0 +1,11 @@
+package org.dows.hep.service;
+
+import org.dows.framework.crud.mybatis.MybatisCrudService;
+import org.dows.hep.entity.CrowdsInstanceEntity;
+
+/**
+ * @author jx
+ * @date 2023/6/15 13:54
+ */
+public interface CrowdsInstanceService extends MybatisCrudService<CrowdsInstanceEntity> {
+}
