524edf3166a6fa22dbb596b3f8bd520e5ee60738
lait <lait.zhang@gmail.com>
2023-06-26 16:00:59 +0800

    fix

--- hep-api/src/main/java/org/dows/hep/api/event/CopyExperimentPersonAndOrgEvent.java
+++ hep-api/src/main/java/org/dows/hep/api/event/CopyExperimentPersonAndOrgEvent.java
@@ -1,20 +1,20 @@
-package org.dows.hep.api.event;
-
-import lombok.Getter;
-
-import java.io.Serializable;
-
-/**
- * @author jx
- * 复制机构与人物事件
- * @date 2023/6/21 11:23
- */
-public class CopyExperimentPersonAndOrgEvent extends ExperimentEvent implements Serializable {
-
-    @Getter
-    private EventName eventName = EventName.copyExperimentPersonAndOrgEvent;
-
-    public CopyExperimentPersonAndOrgEvent(Object source) {
-        super(source);
-    }
-}
+//package org.dows.hep.api.event;
+//
+//import lombok.Getter;
+//
+//import java.io.Serializable;
+//
+///**
+// * @author jx
+// * 复制机构与人物事件
+// * @date 2023/6/21 11:23
+// */
+//public class CopyExperimentPersonAndOrgEvent extends ExperimentEvent implements Serializable {
+//
+//    @Getter
+//    private EventName eventName = EventName.copyExperimentPersonAndOrgEvent;
+//
+//    public CopyExperimentPersonAndOrgEvent(Object source) {
+//        super(source);
+//    }
+//}
