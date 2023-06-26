8b10ad2f58dee51545f03ac797ef10c2632c8925
fenghaibin <2283839410@qq.com>
2023-06-26 11:48:24 +0800

    [FIX] expt-scheme

---
+++ hep-api/src/main/java/org/dows/hep/api/user/experiment/request/ExperimentScheme1Request.java
@@ -0,0 +1,24 @@
+package org.dows.hep.api.user.experiment.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * @author fhb
+ * @version 1.0
+ * @description
+ * @date 2023/6/26 11:44
+ **/
+@Data
+@RequiredArgsConstructor
+@Schema(name = "ExperimentScheme1Request 对象", title = "实验方案设计")
+public class ExperimentScheme1Request {
+    @NotBlank
+    @Schema(title = "实验方案设计ID")
+    private String experimentSchemeItemId;
+
+    @Schema(title = "作答答案")
+    private String questionResult;
+}
