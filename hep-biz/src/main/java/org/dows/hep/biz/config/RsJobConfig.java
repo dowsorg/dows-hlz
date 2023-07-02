327aa8e93cd76a2c5f693a7d95f5db9bef71670d
runsix <runsix1994@gmail.com>
2023-07-02 23:18:56 +0800

    add elasticjob

---
+++ hep-biz/src/main/java/org/dows/hep/biz/config/RsJobConfig.java
@@ -0,0 +1,23 @@
+package org.dows.hep.biz.config;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.apache.shardingsphere.elasticjob.api.JobConfiguration;
+import org.apache.shardingsphere.elasticjob.lite.api.bootstrap.impl.ScheduleJobBootstrap;
+import org.apache.shardingsphere.elasticjob.reg.base.CoordinatorRegistryCenter;
+import org.dows.hep.biz.job.RsJob;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author runsix
+ */
+@Configuration
+@RequiredArgsConstructor
+public class RsJobConfig {
+  private final CoordinatorRegistryCenter coordinatorRegistryCenter;
+
+  @PostConstruct
+  public void job() {
+    new ScheduleJobBootstrap(coordinatorRegistryCenter, new RsJob(), JobConfiguration.newBuilder("RsJob", 3).cron("0/30 * * * * ?").build()).schedule();
+  }
+}
