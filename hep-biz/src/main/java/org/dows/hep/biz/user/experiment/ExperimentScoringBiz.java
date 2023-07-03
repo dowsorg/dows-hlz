8a1929c4e6d5d3276bc2ae6e648d55eee9c687d8
lait <lait.zhang@gmail.com>
2023-07-03 16:57:12 +0800

    fix

---
+++ hep-biz/src/main/java/org/dows/hep/biz/user/experiment/ExperimentScoringBiz.java
@@ -0,0 +1,17 @@
+package org.dows.hep.biz.user.experiment;
+
+/**
+ * 实验计分BIZ
+ * todo
+ * post/healthIndexScoring/健康指数分数计算
+ * post/knowledgeScoring/知识考点分数得分
+ * post/treatmentPercentScoring/医疗占比得分
+ * post/operateRightScoring/操作准确度得分
+ * post//竞争性得分
+ * post/totalScoring/总分
+ * 此处先计算分数，独立的功能点，供其他bean调用，并将生成的数据保存，用于排行和出报告
+ */
+public class ExperimentScoringBiz {
+
+
+}
