232afd2da975658aef398aea120b1dd39ecd5c87
lait <lait.zhang@gmail.com>
2023-07-06 14:56:05 +0800

    fix

---
+++ hep-biz/src/main/java/org/dows/hep/biz/calc/HeroproXxXalculator.java
@@ -0,0 +1,15 @@
+package org.dows.hep.biz.calc;
+
+import org.dows.hep.api.calc.ExperimentScoreCalcRequest;
+
+public class HeroproXxXalculator implements Calculatable {
+    @Override
+    public void calc(ExperimentScoreCalcRequest experimentScoreCalcRequest) {
+
+    }
+
+    @Override
+    public void calc(String experimentInstanceId, String experimentGroupId, Integer period) {
+
+    }
+}
