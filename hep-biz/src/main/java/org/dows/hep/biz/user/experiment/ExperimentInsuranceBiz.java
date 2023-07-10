c2a86254146c23d9146ed174efd422be3dba314d
jiangxia <15228798301@163.com>
2023-07-10 11:10:04 +0800

    fix checkInsureStatus

--- hep-biz/src/main/java/org/dows/hep/biz/user/experiment/ExperimentInsuranceBiz.java
+++ hep-biz/src/main/java/org/dows/hep/biz/user/experiment/ExperimentInsuranceBiz.java
@@ -3,6 +3,7 @@ package org.dows.hep.biz.user.experiment;
 import com.baomidou.dynamic.datasource.annotation.DSTransactional;
 import lombok.RequiredArgsConstructor;
 import org.dows.framework.api.util.ReflectUtil;
+import org.dows.hep.api.exception.ExperimentException;
 import org.dows.hep.api.user.experiment.request.ExperimentPersonInsuranceRequest;
 import org.dows.hep.biz.util.TimeUtil;
 import org.dows.hep.entity.CaseOrgFeeEntity;
@@ -226,6 +227,9 @@ public class ExperimentInsuranceBiz {
                 .eq(ExperimentPersonInsuranceEntity::getOperateOrgId, experimentPersonInsuranceRequest.getOperateOrgId())
                 .eq(ExperimentPersonInsuranceEntity::getDeleted, false)
                 .one();
+        if(entity == null || ReflectUtil.isObjectNull(entity)){
+            throw new ExperimentException("该保险不存在");
+        }
         //判断过期时间
         return TimeUtil.isBeforeTime(new Date(),entity.getExpdate());
     }
