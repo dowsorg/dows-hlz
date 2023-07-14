98e079dafd255362eaddd000de5e3a4e16368132
lait <lait.zhang@gmail.com>
2023-07-14 10:47:50 +0800

    fix:ExperimentRestartTask#实验异常或重启时重启执行一遍实验任务

--- hep-biz/src/main/java/org/dows/hep/biz/task/ExperimentRestartTask.java
+++ hep-biz/src/main/java/org/dows/hep/biz/task/ExperimentRestartTask.java
@@ -4,5 +4,20 @@ package org.dows.hep.biz.task;
  * 实验重启任务
  * 重新初始化调度器
  */
-public class ExperimentRestartTask {
+public class ExperimentRestartTask implements Runnable {
+
+
+    /**
+     * todo
+     * 1.获取实验计时器，
+     * 2.获取experiment_task_schedule
+     * 3.判断experiment_task_schedule.restartTime > experiment_task_schedule.execute_time && executed == '未执行' do 执行原来的任务
+     * 4.判断experiment_task_schedule.restartTime <= experiment_task_schedule.execute_time && executed == '未执行' do 重新拉起定时任务
+     */
+    @Override
+    public void run() {
+
+    }
+
+
 }
