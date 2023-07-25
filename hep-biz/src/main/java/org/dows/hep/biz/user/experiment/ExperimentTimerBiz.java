86cab5adbf6039e45ae2efc51a581f08a8308cea
jiangxia <15228798301@163.com>
2023-07-25 17:49:23 +0800

    add refundFunds

--- hep-biz/src/main/java/org/dows/hep/biz/user/experiment/ExperimentTimerBiz.java
+++ hep-biz/src/main/java/org/dows/hep/biz/user/experiment/ExperimentTimerBiz.java
@@ -10,8 +10,10 @@ import org.dows.hep.api.enums.EnumExperimentMode;
 import org.dows.hep.api.enums.EnumExperimentState;
 import org.dows.hep.api.exception.ExperimentException;
 import org.dows.hep.api.tenant.experiment.request.ExperimentSetting;
+import org.dows.hep.api.user.experiment.request.ExperimentPersonRequest;
 import org.dows.hep.api.user.experiment.response.CountDownResponse;
 import org.dows.hep.api.user.experiment.response.ExperimentPeriodsResonse;
+import org.dows.hep.biz.user.person.PersonStatiscBiz;
 import org.dows.hep.entity.ExperimentInstanceEntity;
 import org.dows.hep.entity.ExperimentParticipatorEntity;
 import org.dows.hep.entity.ExperimentSettingEntity;
@@ -39,6 +41,7 @@ public class ExperimentTimerBiz {
     private final ExperimentInstanceService experimentInstanceService;
     private final ExperimentParticipatorService experimentParticipatorService;
     private final ExperimentSettingService experimentSettingService;
+    private final PersonStatiscBiz personStatiscBiz;
 
 
     /**
@@ -286,6 +289,13 @@ public class ExperimentTimerBiz {
                     countDownResponse.setModel(et.getModel());
                     countDownResponse.setPeriod(et.getPeriod());
                     countDownResponse.setState(et.getState());
+                    if(et.getPeriod() >1){
+                        personStatiscBiz.refundFunds(ExperimentPersonRequest.builder()
+                                                     .experimentInstanceId(experimentInstanceId)
+                                                     .appId("3")
+                                                     .periods(et.getPeriod() - 1)
+                                                     .build());
+                    }
                     break;
                 } else if (sct >= et.getStartTime().getTime() && sct <= et.getEndTime().getTime()) {// 期数中
                     // 本期剩余时间 = 暂停推迟后的结束时间 - 当前时间
