aef13d9dd76c7cd95509071138dab2d5a6b763a7
runsix <runsix1994@gmail.com>
2023-07-26 16:35:14 +0800

    optimize IndicatorExpressionRefBiz.delete CaseIndicatorExpressionRefBiz.delete

--- hep-biz/src/main/java/org/dows/hep/biz/base/indicator/CaseIndicatorExpressionRefBiz.java
+++ hep-biz/src/main/java/org/dows/hep/biz/base/indicator/CaseIndicatorExpressionRefBiz.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.dows.hep.api.enums.EnumESC;
 import org.dows.hep.api.enums.EnumIndicatorExpressionSource;
+import org.dows.hep.api.exception.CaseIndicatorExpressionItemRefException;
 import org.dows.hep.api.exception.CaseIndicatorExpressionRefBizException;
 import org.dows.hep.api.exception.IndicatorExpressionItemRefException;
 import org.dows.hep.entity.*;
@@ -13,6 +14,7 @@ import org.dows.hep.service.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.*;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
@@ -25,11 +27,11 @@ public class CaseIndicatorExpressionRefBiz {
   private final CaseIndicatorExpressionRefService caseIndicatorExpressionRefService;
   private final CaseIndicatorExpressionService caseIndicatorExpressionService;
   private final CaseIndicatorExpressionItemService caseIndicatorExpressionItemService;
-
   private final CaseIndicatorExpressionInfluenceService caseIndicatorExpressionInfluenceService;
+  private final RsUtilBiz rsUtilBiz;
 
   @Transactional(rollbackFor = Exception.class)
-  public void delete(String caseIndicatorExpressionRefId) {
+  public void oldDelete(String caseIndicatorExpressionRefId) {
     CaseIndicatorExpressionRefEntity caseIndicatorExpressionRefEntity = caseIndicatorExpressionRefService.lambdaQuery()
         .eq(CaseIndicatorExpressionRefEntity::getCaseIndicatorExpressionRefId, caseIndicatorExpressionRefId)
         .oneOpt()
@@ -65,6 +67,81 @@ public class CaseIndicatorExpressionRefBiz {
     caseIndicatorExpressionService.remove(
         new LambdaQueryWrapper<CaseIndicatorExpressionEntity>()
             .eq(CaseIndicatorExpressionEntity::getIndicatorExpressionId, caseIndicatorExpressionId)
+    );
+    caseIndicatorExpressionItemService.remove(
+        new LambdaQueryWrapper<CaseIndicatorExpressionItemEntity>()
+            .eq(CaseIndicatorExpressionItemEntity::getIndicatorExpressionId, caseIndicatorExpressionId)
+    );
+  }
+
+  @Transactional(rollbackFor = Exception.class)
+  public void delete(String caseIndicatorExpressionRefId) {
+    /* runsix:delete IndicatorExpressionRefEntity */
+    CaseIndicatorExpressionRefEntity caseIndicatorExpressionRefEntity = caseIndicatorExpressionRefService.lambdaQuery()
+        .eq(CaseIndicatorExpressionRefEntity::getCaseIndicatorExpressionRefId, caseIndicatorExpressionRefId)
+        .oneOpt()
+        .orElseThrow(() -> {
+          log.warn("method CaseIndicatorExpressionRefBiz.delete caseIndicatorExpressionRefId:{} is illegal", caseIndicatorExpressionRefId);
+          throw new CaseIndicatorExpressionItemRefException(EnumESC.VALIDATE_EXCEPTION);
+        });
+    boolean isRemove = caseIndicatorExpressionRefService.remove(
+        new LambdaQueryWrapper<CaseIndicatorExpressionRefEntity>()
+            .eq(CaseIndicatorExpressionRefEntity::getCaseIndicatorExpressionRefId, caseIndicatorExpressionRefId)
+    );
+    if (!isRemove) {
+      log.warn("method CaseIndicatorExpressionRefBiz.delete caseIndicatorExpressionRefId:{} is illegal", caseIndicatorExpressionRefId);
+      throw new IndicatorExpressionItemRefException(EnumESC.VALIDATE_EXCEPTION);
+    }
+
+    /* runsix:delete IndicatorExpressionEntity */
+    String caseIndicatorExpressionId = caseIndicatorExpressionRefEntity.getIndicatorExpressionId();
+    CaseIndicatorExpressionEntity caseIndicatorExpressionEntity = caseIndicatorExpressionService.lambdaQuery()
+        .eq(CaseIndicatorExpressionEntity::getIndicatorExpressionId, caseIndicatorExpressionId)
+        .one();
+    if (Objects.isNull(caseIndicatorExpressionEntity)) {return;}
+    Integer source = caseIndicatorExpressionEntity.getSource();
+    EnumIndicatorExpressionSource enumIndicatorExpressionSource = EnumIndicatorExpressionSource.getBySource(source);
+    /* runsix:如果是指标管理中指标产生的公式，需要对影响进行处理 */
+    if (enumIndicatorExpressionSource == EnumIndicatorExpressionSource.INDICATOR_MANAGEMENT) {
+      /* runsix:指标本体 */
+      String casePrincipalId = caseIndicatorExpressionEntity.getCasePrincipalId();
+      List<CaseIndicatorExpressionItemEntity> caseIndicatorExpressionItemEntityList = caseIndicatorExpressionItemService.lambdaQuery()
+          .eq(CaseIndicatorExpressionItemEntity::getIndicatorExpressionId, caseIndicatorExpressionId)
+          .list();
+      /* runsix:影响这个指标的指标id */
+      Set<String> caseInfluencedIndicatorInstanceIdSet = new HashSet<>();
+      caseIndicatorExpressionItemEntityList.forEach(caseIndicatorExpressionItemEntity -> {
+        String conditionValList = caseIndicatorExpressionItemEntity.getConditionValList();
+        String resultValList = caseIndicatorExpressionItemEntity.getResultValList();
+        caseInfluencedIndicatorInstanceIdSet.addAll(rsUtilBiz.getConditionValSplitList(conditionValList));
+        caseInfluencedIndicatorInstanceIdSet.addAll(rsUtilBiz.getResultValSplitList(resultValList));
+      });
+      /* runsix:移除它自己 */
+      caseInfluencedIndicatorInstanceIdSet.remove(casePrincipalId);
+      /* runsix:所有需要改的，包括它自己和影响它的 */
+      List<CaseIndicatorExpressionInfluenceEntity> allCaseIndicatorExpressionInfluenceEntityList = new ArrayList<>();
+      CaseIndicatorExpressionInfluenceEntity casePrincipalIndicatorExpressionInfluenceEntity = caseIndicatorExpressionInfluenceService.lambdaQuery()
+          .eq(CaseIndicatorExpressionInfluenceEntity::getIndicatorInstanceId, casePrincipalId)
+          .one();
+      casePrincipalIndicatorExpressionInfluenceEntity.setInfluencedIndicatorInstanceIdList(null);
+      allCaseIndicatorExpressionInfluenceEntityList.add(casePrincipalIndicatorExpressionInfluenceEntity);
+
+      List<CaseIndicatorExpressionInfluenceEntity> caseIndicatorExpressionInfluenceEntityList = caseIndicatorExpressionInfluenceService.lambdaQuery()
+          .in(CaseIndicatorExpressionInfluenceEntity::getIndicatorInstanceId, caseInfluencedIndicatorInstanceIdSet)
+          .list();
+      caseIndicatorExpressionInfluenceEntityList.forEach(caseIndicatorExpressionInfluenceEntity -> {
+        String influenceIndicatorInstanceIdList = caseIndicatorExpressionInfluenceEntity.getInfluenceIndicatorInstanceIdList();
+        List<String> originList = rsUtilBiz.getSplitList(influenceIndicatorInstanceIdList);
+        /* runsix:减去此次删除的指标id */
+        originList.remove(casePrincipalId);
+        caseIndicatorExpressionInfluenceEntity.setInfluenceIndicatorInstanceIdList(rsUtilBiz.getCommaList(originList));
+        allCaseIndicatorExpressionInfluenceEntityList.add(caseIndicatorExpressionInfluenceEntity);
+      });
+      caseIndicatorExpressionInfluenceService.saveOrUpdateBatch(allCaseIndicatorExpressionInfluenceEntityList);
+    }
+    caseIndicatorExpressionService.remove(
+        new LambdaQueryWrapper<CaseIndicatorExpressionEntity>()
+            .eq(CaseIndicatorExpressionEntity::getCaseIndicatorExpressionId, caseIndicatorExpressionId)
     );
     caseIndicatorExpressionItemService.remove(
         new LambdaQueryWrapper<CaseIndicatorExpressionItemEntity>()
