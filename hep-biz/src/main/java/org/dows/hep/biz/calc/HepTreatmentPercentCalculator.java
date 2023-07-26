9f6bf9922660f57fcb7b293b9fb81abdff82d2aa
lait <lait.zhang@gmail.com>
2023-07-26 10:30:22 +0800

    fix:ExperimentScoringBiz.java     HepTreatmentPercentCalculator.java     OperateCostBiz.java#医疗占比

--- hep-biz/src/main/java/org/dows/hep/biz/calc/HepTreatmentPercentCalculator.java
+++ hep-biz/src/main/java/org/dows/hep/biz/calc/HepTreatmentPercentCalculator.java
@@ -1,14 +1,27 @@
 package org.dows.hep.biz.calc;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.dows.hep.api.calc.ExperimentScoreCalcRequest;
+import org.dows.hep.biz.operate.CostRequest;
+import org.dows.hep.biz.operate.OperateCostBiz;
 import org.springframework.stereotype.Component;
 
+/**
+ * 医疗占比计算器
+ */
+@Slf4j
+@RequiredArgsConstructor
 @Component("hepTreatmentPercentCalculator")
 public class HepTreatmentPercentCalculator implements Calculatable {
 
 
+    private final OperateCostBiz operateCostBiz;
     @Override
     public void calc(ExperimentScoreCalcRequest experimentScoreCalcRequest) {
+
+        CostRequest costRequest = CostRequest.builder().build();
+        operateCostBiz.calcGroupTreatmentPercent(costRequest);
 
     }
 
