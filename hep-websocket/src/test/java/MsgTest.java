2c3e9e63f14f365a08fa93de499505a7b4a47862
lait <lait.zhang@gmail.com>
2023-08-08 19:23:43 +0800

    fix:ws

--- hep-websocket/src/test/java/MsgTest.java
+++ hep-websocket/src/test/java/MsgTest.java
@@ -14,7 +14,7 @@ public class MsgTest {
         ConcurrentMap<Channel, AccountInfo> userInfos = HepClientManager.getUserInfos();
         Set<Channel> channels = userInfos.keySet();
         for (Channel channel : channels) {
-            HepClientManager.sendInfoRetry(channel, MessageCode.MESS_CODE, Response.ok("ok"),null);
+            HepClientManager.sendInfoRetry(channel, MessageCode.MESS_CODE, Response.ok("ok"), "", null);
         }
     }
 }
