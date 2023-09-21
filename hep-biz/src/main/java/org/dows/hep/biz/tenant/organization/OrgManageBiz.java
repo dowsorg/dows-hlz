ccd505b0c441e53c58e95e4060cd50be0561a361
zhaoyitao <zhaoyitao@findsoft.com>
2023-09-21 21:26:17 +0800

    两种Excel都可以导入

--- hep-biz/src/main/java/org/dows/hep/biz/tenant/organization/OrgManageBiz.java
+++ hep-biz/src/main/java/org/dows/hep/biz/tenant/organization/OrgManageBiz.java
@@ -14,6 +14,7 @@ import org.dows.hep.biz.tenant.excel.BatchInsertBiz;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -120,7 +121,10 @@ public class OrgManageBiz{
         Integer failCount = 0;
         InputStream fin = null;
         try {
-            fin = file.getInputStream();
+//            fin = file.getInputStream();
+            File newFile = batchInsertBiz.multipartFileToFile(file);
+            fin =  batchInsertBiz.parseImportExcelStream(newFile);
+            newFile.delete();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
