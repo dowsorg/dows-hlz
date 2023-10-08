b7eda058c11fde4ef9f90eec644b8a996c18f4ea
zhaoyitao <zhaoyitao@findsoft.com>
2023-10-08 18:08:34 +0800

    2.人员管理接口优化

--- hep-biz/src/main/java/org/dows/hep/biz/base/extuim/AccountInstanceExtBiz.java
+++ hep-biz/src/main/java/org/dows/hep/biz/base/extuim/AccountInstanceExtBiz.java
@@ -4,17 +4,11 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.lang3.StringUtils;
 import org.dows.account.entity.AccountInstance;
-import org.dows.account.request.AccountInstanceRequest;
-import org.dows.account.response.AccountInstanceResponse;
 import org.dows.account.service.AccountInstanceService;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 /**
@@ -43,28 +37,4 @@ public class AccountInstanceExtBiz {
         return accountIds;
     }
 
-    public List<AccountInstanceResponse> getAccountInstanceList1(AccountInstanceRequest request) {
-        List<AccountInstance> voList =
-                this.accountInstanceService.lambdaQuery()
-                        .like(StringUtils.isNotEmpty(request.getAccountId()), AccountInstance::getAccountId, request.getAccountId())
-                        .like(StringUtils.isNotEmpty(request.getAccountName()), AccountInstance::getAccountName, request.getAccountName())
-                        .eq(StringUtils.isNotEmpty(request.getSource()), AccountInstance::getSource, request.getSource())
-                        .like(StringUtils.isNotEmpty(request.getPhone()), AccountInstance::getPhone, request.getPhone())
-                        .eq(StringUtils.isNotEmpty(request.getAppId()), AccountInstance::getAppId, request.getAppId())
-                        .eq(request.getStatus() != null, AccountInstance::getStatus, request.getStatus())
-                        .eq(request.getDt() != null, AccountInstance::getDt, request.getDt())
-                        .gt(request.getStartTime() != null, AccountInstance::getDt, request.getStartTime())
-                        .lt(request.getEndTime() != null, AccountInstance::getDt, request.getEndTime())
-                        .orderByDesc(AccountInstance::getDt)
-                        .list();
-        List<AccountInstanceResponse> list = new ArrayList<>();
-        voList.forEach((vo) -> {
-            AccountInstanceResponse model = new AccountInstanceResponse();
-            BeanUtils.copyProperties(vo, model);
-            model.setId(vo.getId().toString());
-            list.add(model);
-        });
-
-        return list;
-    }
 }
